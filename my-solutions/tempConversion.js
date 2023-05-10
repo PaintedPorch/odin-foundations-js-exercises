312d8a88380fdb35b0e95e457173a91a3a5690c2
Lars Bakker <Lars.bakker@casema.nl>
2023-05-10 09:54:01 +0200

    Add tempConversion.js

---
+++ my-solutions/tempConversion.js
@@ -0,0 +1,19 @@
+const convertToCelsius = function(fahrenheit) {
+  let result = (fahrenheit - 32) / 1.8;
+  result = Math.round(result * 10) / 10;
+
+  return result;
+};
+
+const convertToFahrenheit = function(celsius) {
+  let result = (celsius * 1.8) + 32;
+  result = Math.round(result * 10) / 10;
+
+  return result;
+};
+
+// Do not edit below this line
+module.exports = {
+  convertToCelsius,
+  convertToFahrenheit
+};
